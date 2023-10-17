b1bd1f6a3dd483c360337ef428d4e46ccaa510fe
Sakshi Belsare <sakshibelsare58@gmail.com>
2023-10-17 18:42:46 +0530

    Updated project links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire/compat'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,11 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  //   provideFirebaseApp(() => initializeApp(environment.firebase)),
+  //   provideAuth(() => getAuth()),
+  //   provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyCzO2odAjPmHaQokG77q1-x_e_xSkh2nZk",
+      authDomain: "mindfit-f89d6.firebaseapp.com",
+      // databaseURL: "https://mindfit-f89d6-default-rtdb.firebaseio.com",
+      projectId: "mindfit-f89d6",
+      storageBucket: "mindfit-f89d6.appspot.com",
+      messagingSenderId: "596801740362",
+      appId: "1:596801740362:web:4679e8e8cd6ba5ea7210b3",
+      // measurementId: "G-Q5JDCWDBED"
+
+  },
+};
