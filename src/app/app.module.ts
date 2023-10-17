5985428a025ab3a5d72402fab7e217a00eafc09f
Atharva <Gadekaratharva02@gmail.com>
2023-10-17 16:11:53 +0530

    The commit removes the AngularFireModule import. (generated by useblackbox.ai )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AngularFireModule} from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
 
 @NgModule({
   declarations: [
@@ -12,6 +22,9 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
   ],
   imports: [
     BrowserModule,
+    
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
